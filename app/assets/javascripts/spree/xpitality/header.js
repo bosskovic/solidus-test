9792ede02c4c98be01993f15ac7a6623f5a5400a
Vishal <vishal@xpitality.com>
2021-08-06 17:17:33 +0530

    revert menu style changes and resolve click issue

--- app/assets/javascripts/spree/xpitality/header.js
+++ app/assets/javascripts/spree/xpitality/header.js
@@ -8,19 +8,22 @@ const handleHeader = () => {
   const dropdown = document.querySelectorAll(".nav-dropdown");
   const searchWrapper = document.querySelector(".header-content");
   const navHolder = document.querySelectorAll(".nav-link");
-  const CheckoutPage = document.querySelector('.checkout');
-  const CheckoutPageLogin = document.querySelector('.login');
+  const CheckoutPage = document.querySelector(".checkout");
+  const CheckoutPageLogin = document.querySelector(".login");
   const navbar = document.querySelector(".navbar");
-  const catalogHolder = document.querySelector('.catalog-holder');
+  const catalogHolder = document.querySelector(".catalog-holder");
+  const body = document.querySelector("body");
 
   hamburger.addEventListener("click", () => {
     if (navBar.classList.contains("show")) {
       navBar.classList.remove("show");
-      searchHolder.style.display = 'flex';
+      searchHolder.style.display = "flex";
       searchWrapper.style.height = "163px";
+      body.style.overflow = "auto";
     } else {
       navBar.classList.add("show");
-      searchHolder.style.display = 'none';
+      searchHolder.style.display = "none";
+      body.style.overflow = "hidden";
     }
   });
 
@@ -29,8 +32,8 @@ const handleHeader = () => {
     link.addEventListener("mouseover", (event) => {
       const allDrops = [...document.querySelectorAll(".nav-dropdown")];
       const drop = event.target.parentElement.querySelector(".nav-dropdown");
-
-      if(drop){
+      const windowWidth = $(window).width();
+      if (drop && windowWidth > 1024) {
         if (drop.classList.contains("show")) {
           drop.classList.remove("show");
         } else {
@@ -43,7 +46,30 @@ const handleHeader = () => {
     });
     link.addEventListener("click", (e) => {
       e.preventDefault();
-      return false;
+      const windowWidth = $(window).width();
+      if (windowWidth > 1024) {
+        return false;
+      } else {
+        const allDrops = [...document.querySelectorAll(".nav-dropdown")];
+        const allLinks = [...document.querySelectorAll(".nav-link")];
+        const drop = e.target.parentElement.querySelector(".nav-dropdown");
+
+        if (drop) {
+          if (drop.classList.contains("show")) {
+            drop.classList.remove("show");
+            link.classList.remove("open");
+          } else {
+            allDrops.forEach((el) => {
+              el.classList.remove("show");
+            });
+            allLinks.forEach((el) => {
+              el.classList.remove("open");
+            });
+            drop.classList.add("show");
+            link.classList.add("open");
+          }
+        }
+      }
     });
   });
 
@@ -53,16 +79,16 @@ const handleHeader = () => {
       drop.classList.remove("show");
     });
   });
-  navHolder.forEach((link)=>{
-    link.addEventListener("mouseover",()=>{
-      if(!link.classList.contains('nav-link-with-drop')){
+  navHolder.forEach((link) => {
+    link.addEventListener("mouseover", () => {
+      if (!link.classList.contains("nav-link-with-drop")) {
         const allDrops = [...document.querySelectorAll(".nav-dropdown")];
         allDrops.forEach((el) => {
           el.classList.remove("show");
         });
       }
-    })
-  })
+    });
+  });
 
   //Handling regions sidebar
   titleOrigin.forEach((title) => {
@@ -82,25 +108,25 @@ const handleHeader = () => {
   const isMobile = /iPhone|Android/i.test(navigator.userAgent);
   if (isMobile) {
     if (CheckoutPage || CheckoutPageLogin) {
-      searchHolder.style.display = 'none';
+      searchHolder.style.display = "none";
       searchWrapper.style.height = "88px";
     }
     window.onscroll = function () {
       if (window.scrollY > 100) {
-        searchHolder.style.display = 'none';
+        searchHolder.style.display = "none";
         searchWrapper.style.height = "88px";
-        if(catalogHolder){
-            navbar.style.height = "100%";
-            navbar.style.top = "64px";
+        if (catalogHolder) {
+          navbar.style.height = "100%";
+          navbar.style.top = "64px";
         }
-      } else if (window.scrollY == 0){
+      } else if (window.scrollY == 0) {
         if (CheckoutPage || CheckoutPageLogin) {
-          searchHolder.style.display = 'none';
+          searchHolder.style.display = "none";
           searchWrapper.style.height = "88px";
-        } else if (catalogHolder){
+        } else if (catalogHolder) {
           navbar.style.top = "121px";
         } else {
-          searchHolder.style.display = 'flex';
+          searchHolder.style.display = "flex";
           searchWrapper.style.height = "163px";
         }
       }
