e672734394bb16ddc19d0a04a49399bce08a4f74
Marko Bošković <bosskovic@gmail.com>
2021-08-13 13:32:32 +0200

    sets memcached as cachestore [ch11067]

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -59,7 +59,7 @@ Rails.application.configure do
   config.log_tags = [ :request_id ]
 
   # Use a different cache store in production.
-  # config.cache_store = :mem_cache_store
+  config.cache_store = :mem_cache_store, "enoteca-naturale-memcached.lfs5kr.cfg.euc1.cache.amazonaws.com:11211"
 
   # Use a real queuing backend for Active Job (and separate queues per environment).
   # config.active_job.queue_adapter     = :resque
